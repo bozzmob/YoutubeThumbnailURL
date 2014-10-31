11cb9fa1c114c9863f878a641c1d8267d996685d
satvik <satvik.jagannath7@gmail.com>
2014-11-01 00:58:57 +0530

    Script v2

--- getThumbnailURL.sh
+++ getThumbnailURL.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
-consturl="https://www.youtube.com/user/"
-echo $consturl
-echo $1
-consturl=$consturl$1
-echo $consturl
-wget $consturl
-imgurl=$(grep "appbar-nav-avatar" $1 | grep -o 'https.*jpg')
-echo $1"\t"$imgurl >> output.txt
-rm $1
+opfile=$1output.json
+echo "{ \"channels\" : [" >> $opfile
+while read line
+do
+	consturl="https://www.youtube.com/user/"
+	echo $consturl
+	echo $line
+	consturl=$consturl$line
+	echo $consturl
+	wget $consturl
+	imgurl=$(grep "appbar-nav-avatar" $line | grep -o 'https.*jpg')
+#	title=$(grep "branded-page-header-title-link" $line | grep -io 'title=".*"')
+	echo $imgurl
+        title=$(grep "<title>" $line)
+	echo $title  >> title.txt
+#	echo -e "{\"userid\":\""$line"\", \"title\":\""$title"\", \"imgurl\":\""$imgurl"\"}" >> output.txt
+        echo "{\"userid\":\""$line"\", $title\", \"imgurl\":\""$imgurl"\"}," >> $opfile
+#        echo -e "{\"userid\":\""$line"\", \"title\":\""$title"\", \"imgurl\":\""$imgurl"\"}" >> output.txt
+	rm $line
+done < $1
+echo "]}" >> $opfile
+cat $opfile
+sed -i 's/<title>  /"title":"/g' $opfile
+sed -i 's/""/"\/images\/default.png"/g' $opfile
+cat $opfile
+#echo $1"\t"$imgurl >> output.txt
